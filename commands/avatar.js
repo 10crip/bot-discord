15f767abbbbdfb30075f88ded89244af8b5addd5
10crip <kbsuicidajr@gmail.com>
2026-04-07 02:20:33 -0300

    Add files via upload

---
+++ commands/avatar.js
@@ -0,0 +1,16 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'avatar',
+    execute(message) {
+        const usuario = message.mentions.users.first() || message.author;
+
+        const embed = new EmbedBuilder()
+            .setTitle(`🖼️ Avatar de ${usuario.username}`)
+            .setImage(usuario.displayAvatarURL({ dynamic: true, size: 1024 }))
+            .setColor('Blue')
+            .setFooter({ text: `Solicitado por ${message.author.username}` });
+
+        message.reply({ embeds: [embed] });
+    }
+};
