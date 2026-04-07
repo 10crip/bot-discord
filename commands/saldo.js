01e2af3f921506c434eecc6e9c4138ac9ac24c71
10crip <kbsuicidajr@gmail.com>
2026-04-07 02:10:23 -0300

    saldo.js

--- commands/saldo.js
+++ commands/saldo.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { EmbedBuilder } = require('discord.js');
 const db = require('../database.json');
 
 module.exports = {
@@ -6,10 +7,17 @@ module.exports = {
     execute(message) {
         const id = message.author.id;
 
-        if (!db[id]) db[id] = { money: 0 };
+        if (!db[id]) {
+            db[id] = { money: 0 };
+            fs.writeFileSync('./database.json', JSON.stringify(db, null, 2));
+        }
 
-        fs.writeFileSync('./database.json', JSON.stringify(db, null, 2));
+        const embed = new EmbedBuilder()
+            .setTitle('💰 Carteira')
+            .setDescription(`${message.author}, seu saldo atual é **${db[id].money} moedas**.`)
+            .setColor('Gold')
+            .setThumbnail(message.author.displayAvatarURL());
 
-        message.reply(`💰 Seu saldo: ${db[id].money}`);
+        message.reply({ embeds: [embed] });
     }
 };
