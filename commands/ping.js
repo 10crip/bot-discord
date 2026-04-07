4cfbabf7ade18340157c8d1639905db6ad8627b4
10crip <kbsuicidajr@gmail.com>
2026-04-07 02:08:43 -0300

    ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,14 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'ping',
-    execute(message) {
-        message.reply('🏓 Pong!');
+    async execute(message) {
+        const embed = new EmbedBuilder()
+            .setTitle('🏓 Pong!')
+            .setDescription(`Latência da API: **${message.client.ws.ping}ms**`)
+            .setColor('Red')
+            .setFooter({ text: `Solicitado por ${message.author.username}` });
+
+        message.reply({ embeds: [embed] });
     }
 };
