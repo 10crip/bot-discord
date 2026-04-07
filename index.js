88221a10dfb5fd6acb313643d98cc37db381c09f
10crip <kbsuicidajr@gmail.com>
2026-04-07 06:15:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
 const fs = require('fs');
+const path = require('path');
 
 const client = new Client({
     intents: [
@@ -15,14 +16,32 @@ const client = new Client({
 
 client.commands = new Collection();
 
-fs.readdirSync('./commands').forEach(file => {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-});
+const commandsPath = path.join(__dirname, 'commands');
+if (fs.existsSync(commandsPath)) {
+    fs.readdirSync(commandsPath)
+        .filter(file => file.endsWith('.js'))
+        .forEach(file => {
+            const command = require(`./commands/${file}`);
+            if (command?.name) {
+                client.commands.set(command.name, command);
+            }
+        });
+}
 
-fs.readdirSync('./events').forEach(file => {
-    const event = require(`./events/${file}`);
-    client.on(event.name, (...args) => event.execute(...args, client));
+const eventsPath = path.join(__dirname, 'events');
+if (fs.existsSync(eventsPath)) {
+    fs.readdirSync(eventsPath)
+        .filter(file => file.endsWith('.js'))
+        .forEach(file => {
+            const event = require(`./events/${file}`);
+            if (!event?.name || !event?.execute) return;
+
+            client.on(event.name, (...args) => event.execute(...args, client));
+        });
+}
+
+client.once('ready', () => {
+    console.log(`✅ Bot online como ${client.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
